2e375c0136ac566ff75d8d071dc4b8b97c4455f9
Atsuhiko Kimura <atskimura@gmail.com>
2020-07-22 16:09:56 +0900

    Improve error handling

--- functions/proxy.js
+++ functions/proxy.js
@@ -32,38 +32,47 @@ module.exports = function(options) {
   options = options || {};
 
   return function(req, res) {
-    if (options.enableCORS) {
-      res.header('Access-Control-Allow-Origin', options.allowedOrigin || '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE');
-      res.header('Access-Control-Allow-Headers', ALLOWED_HEADERS.join(','));
-      res.header('Access-Control-Expose-Headers', 'SForce-Limit-Info');
-      if (req.method === 'OPTIONS') {
-        res.end();
+    try {
+      if (options.enableCORS) {
+        res.header('Access-Control-Allow-Origin', options.allowedOrigin || '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE');
+        res.header('Access-Control-Allow-Headers', ALLOWED_HEADERS.join(','));
+        res.header('Access-Control-Expose-Headers', 'SForce-Limit-Info');
+        if (req.method === 'OPTIONS') {
+          res.end();
+          return;
+        }
+      }
+      const sfEndpoint = req.headers["salesforceproxy-endpoint"];
+      console.log(sfEndpoint);
+      if (!SF_ENDPOINT_REGEXP.test(sfEndpoint)) {
+        res.send(400, "Proxying endpoint is not allowed. `salesforceproxy-endpoint` header must be a valid Salesforce domain: " + sfEndpoint);
         return;
       }
-    }
-    const sfEndpoint = req.headers["salesforceproxy-endpoint"];
-    if (!SF_ENDPOINT_REGEXP.test(sfEndpoint)) {
-      res.send(400, "Proxying endpoint is not allowed. `salesforceproxy-endpoint` header must be a valid Salesforce domain: " + sfEndpoint);
-      return;
-    }
-    let headers = {};
-    ALLOWED_HEADERS.forEach(header => {
-      header = header.toLowerCase();
-      const value = req.headers[header]
-      if (value) {
-        const name = header === 'x-authorization' ? 'authorization' : header;
-        headers[name] = req.headers[header];
+      let headers = {};
+      ALLOWED_HEADERS.forEach(header => {
+        header = header.toLowerCase();
+        const value = req.headers[header]
+        if (value) {
+          const name = header === 'x-authorization' ? 'authorization' : header;
+          headers[name] = req.headers[header];
+        }
+      });
+      const params = {
+        url: sfEndpoint || "https://login.salesforce.com//services/oauth2/token",
+        method: req.method,
+        headers: headers,
+      };
+      if (req.method !== 'GET' && req.body) {
+        params.body = req.body;
       }
-    });
-    const params = {
-      url: sfEndpoint || "https://login.salesforce.com//services/oauth2/token",
-      method: req.method,
-      headers: headers,
-    };
-    if (req.method !== 'GET' && req.body) {
-      params.body = req.body;
+      request(params).on('error', err => {
+        console.error(err);
+        res.status(500).send(JSON.stringify(err));
+      }).pipe(res);
+    } catch(e) {
+      console.error(e);
+      res.status(500).send(JSON.stringify(e));
     }
-    request(params).pipe(res);
   }
 };
